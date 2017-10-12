b2e26bf649a0bce6f86a2d34f7aee7aa4c6f0654
Mulia Arifandi Nasution <mul14.net@gmail.com>
2017-10-12 19:06:27 +0700

    Add Str helper

---
+++ src/helpers/Str.js
@@ -0,0 +1,11 @@
+export default class Str {
+  static random (length = 16) {
+    let text = ''
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+
+    for (let i = 0; i < length; i++)
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text
+  }
+}
