e71c4fcf46a5e2d3045d4178f43bd557cd52d981
Mulia Arifandi Nasution <mul14.net@gmail.com>
2017-10-13 09:42:34 +0700

    Save

--- src/components/Inventory/StockAdjusment/Form.vue
+++ src/components/Inventory/StockAdjusment/Form.vue
@@ -18,7 +18,8 @@
                   <div class="form-group form-general m-b-20">
                     <label class="col-md-2 control-label text-left">Adjustment ID</label>
                     <div class="col-md-3">
-                      <input v-model="form.adjustment_id" disabled required type="text" placeholder="" class="form-control">
+                      <input v-model="form.stock_adjustment_id" disabled required type="text" placeholder=""
+                             class="form-control">
                     </div>
                   </div>
                   <div class="form-group form-general m-b-20">
@@ -76,56 +77,53 @@
                               </tr>
                               </thead>
                               <tbody>
-                              <tr v-for="item in list.items">
-                                <td>{{ item.item_id }}</td>
+                              <tr v-for="detail in form.details">
+                                <td>{{ detail.item_id }}</td>
                                 <td>
                                   <div class="col-md-12 pl-pr-0">
-                                    <select class="form-control">
-                                      <!--<option v-for="item in list.items" :value="">-->
-                                        <!--{{ item.name }}-->
-                                      <!--</option>-->
+                                    <select class="form-control" v-model="detail.item_id">
+                                      <option v-for="item in list.items" :value="item.item_id">
+                                        {{ item.item_name }}
+                                      </option>
                                     </select>
                                   </div>
                                 </td>
                                 <td align="center">
-                                  {{ item.database_qty }}
+                                  {{ detail.database_qty }}
                                 </td>
                                 <td>
                                   <input
-                                    v-model="item.adjust_qty"
-                                    class="vertical-spin"
-                                    type="number"
-                                    value=""
-                                    name="vertical-spin"
-                                    data-bts-button-down-class="btn btn-primary"
-                                    data-bts-button-up-class="btn btn-primary">
-                                </td>
-                                <td>
-                                  <input
-                                    v-model="item.on_hand_qty"
-                                    class="vertical-spin"
+                                    v-model="detail.adjust_qty"
+                                    @keyup="changeOnHandValue(detail)"
+                                    class="vertical-spin form-control"
                                     type="number"
                                     name="vertical-spin"
                                     data-bts-button-down-class="btn btn-primary"
                                     data-bts-button-up-class="btn btn-primary">
                                 </td>
                                 <td>
-                                  <select class="form-control" v-model="item.reason_id">
+                                  <input
+                                    v-model.number="detail.on_hand_qty"
+                                    @keyup="changeAdjustValue(detail)"
+                                    class="vertical-spin form-control"
+                                    type="number"
+                                    name="vertical-spin"
+                                    data-bts-button-down-class="btn btn-primary"
+                                    data-bts-button-up-class="btn btn-primary">
+                                </td>
+                                <td>
+                                  <select class="form-control" v-model="detail.reason_id">
                                     <option v-for="reason in list.reasons" :value="reason.reason_id">
                                       {{ reason.reason }}
                                     </option>
                                   </select>
                                 </td>
                               </tr>
-                              <tr>
-                                <td colspan="2">
-                                  <button type="button" @click="addNew" class="btn-link">
-                                    + Add another line
-                                  </button>
-                                </td>
-                              </tr>
                               </tbody>
                             </table>
+                            <button type="button" @click="addNew" class="btn-link">
+                              + Add another line
+                            </button>
                           </div>
                         </div>
                       </div>
@@ -144,11 +142,13 @@
               <div class="col-md-6 text-right">
                 <div class="col-md-6 col-md-offset-6">
                   <div class="btn-group dropup">
-                    <button type="button" class="btn btn-primary waves-effect waves-light">Save as Draft</button>
+                    <button @click="saveAsDraft" type="button" class="btn btn-primary waves-effect waves-light">
+                      Save as Draft
+                    </button>
                     <button type="button" class="btn btn-primary dropdown-toggle waves-effect waves-light"
                             data-toggle="dropdown" aria-expanded="false"><i class="caret"></i></button>
                     <ul class="dropdown-menu" role="menu">
-                      <li><a href="#">Save and Close</a></li>
+                      <li><a @click="saveAndClose" href="javascript:void(0)">Save and Close</a></li>
                     </ul>
                   </div>
                 </div>
@@ -170,6 +170,21 @@
   export default {
     name: 'StockAdjustmentForm',
 
+    watch: {
+      'form.details.adjust_qty': {
+        handler (val, oldVal) {
+          console.log(_.cloneDeep(val))
+        },
+        deep: true,
+      },
+      'form.details.on_hand_qty': {
+        handler (val, oldVal) {
+          console.log(_.cloneDeep(val))
+        },
+        deep: true,
+      },
+    },
+
     data () {
       return {
         list: {
@@ -177,7 +192,7 @@
           reasons: [],
         },
         form: new Form({
-          adjustment_id: Str.random(),
+          stock_adjustment_id: null,
           stock_adjustment_date: new Date().toISOString().substr(0, 10),
           reference_number: '',
           is_applied: false,
@@ -194,7 +209,16 @@
     },
 
     mounted () {
+      // Edit mode
+      const stockId = this.$route.params.id
+      if (!stockId) {
+        this.form.adjustment_id = Str.random()
+      } else {
+        this.getDetails(stockId)
+      }
+
       this.initialize()
+      this.getItems()
 
       $('#adjustment_date_picker').flatpickr({
         altInput: true,
@@ -202,15 +226,63 @@
     },
 
     methods: {
+
       async initialize () {
         const res = await Axios.get(`stock_adjustments/create`)
-
         this.list.reasons = res.data.data.reasons
       },
 
-      addNew () {
-        this.list.items.push({})
+      async getItems () {
+        const res = await Axios.get(`items`, {
+          params: {
+            per_page: 9999,
+          },
+        })
+        this.list.items = res.data.data
       },
+
+      async getDetails (stockId) {
+        const res = await Axios.get(`stock_adjustments/${stockId}`)
+        this.form.details = res.data.data.details
+        this.form.details[0].database_qty = 129
+        this.form.stock_adjustment_id = res.data.data.stock_adjustment_id
+      },
+
+      addNew () {
+        this.form.details.push({})
+      },
+
+      async saveAsDraft () {
+        try {
+          this.form.is_applied = false
+          this.form.is_void = false
+          const res = await Axios.post(`stock_adjustments`, this.form)
+        }
+        catch (err) {
+          console.error(err)
+        }
+      },
+
+      async saveAndClose () {
+        try {
+          this.form.is_applied = true
+          this.form.is_void = false
+          const res = await Axios.post(`stock_adjustments`, this.form)
+          console.log(_.cloneDeep(res.data))
+        }
+        catch (err) {
+          console.error(err)
+        }
+      },
+
+      changeAdjustValue (detail) {
+        detail.adjust_qty =  parseInt(detail.on_hand_qty) - parseInt(detail.database_qty)
+      },
+
+      changeOnHandValue (detail) {
+        detail.on_hand_qty = parseInt(detail.database_qty) + parseInt(detail.adjust_qty)
+      },
+
     },
   }
 </script>
