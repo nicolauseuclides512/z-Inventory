6dcac2229d05317bc0bf6d4bd18129b1b5fcb581
Mulia Arifandi Nasution <mul14.net@gmail.com>
2017-10-17 07:56:27 +0700

    Fix breaking changes because of vue-loader 13
    I've set `esModule: false` to vue-loader config.
    
    Read this for more information
    https://github.com/vuejs/vue-loader/releases/tag/v13.0.0

--- build/vue-loader.conf.js
+++ build/vue-loader.conf.js
@@ -3,6 +3,7 @@ var config = require('../config')
 var isProduction = process.env.NODE_ENV === 'production'
 
 module.exports = {
+  esModule: false, // Disable on vue-loader 13 - https://github.com/vuejs/vue-loader/releases/tag/v13.0.0
   loaders: utils.cssLoaders({
     sourceMap: isProduction
       ? config.build.productionSourceMap
