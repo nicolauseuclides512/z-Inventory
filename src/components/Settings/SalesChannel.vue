dea647469fb86cca937f5f1a47a0239c72ede2fc
Mulia Arifandi Nasution <mul14.net@gmail.com>
2017-11-27 11:59:05 +0700

    Sync modal

--- src/components/Settings/SalesChannel.vue
+++ src/components/Settings/SalesChannel.vue
@@ -57,7 +57,7 @@
                   <th></th>
                   <th>Channel</th>
                   <th>Store Name</th>
-                  <th></th>
+                  <th colspan="2"></th>
                 </tr>
                 </thead>
                 <tbody>
@@ -67,6 +67,11 @@
                   </td>
                   <td>{{ channel.sales_channel.channel_name }}</td>
                   <td>{{ channel.store_name }}</td>
+                  <td>
+                    <div v-if="channel.sales_channel.channel_name.toLowerCase() === 'lazada'">
+                      <button class="btn btn-default waves-effect" @click="syncModalToggle">Syncronize to Lazada</button>
+                    </div>
+                  </td>
                   <td>
                     <button @click="remove(channel)" type="button" class="btn btn-default waves-effect">
                       <i class="ion-trash-b"></i>
@@ -80,6 +85,49 @@
         </div>
       </div>
     </div>
+
+    <!-- Sync modal -->
+    <div id="lazada" v-if="ui.showSyncModal" class="modal fade in" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" style="display: block; padding-right: 8px;">
+      <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" @click="syncModalToggle" data-dismiss="modal" aria-hidden="true">×</button>
+            <h4 class="modal-title">Synchronize to Marketplace</h4>
+          </div>
+          <div class="modal-body">
+            <div class="row">
+              <div class="col-md-12">
+                <h5 class="title"><center>SELLER CENTER LAZADA</center></h5>
+                <div class="form-horizontal">
+                  <form class="form-horizontal m-t-20" action="index.html">
+                    <div class="form-group">
+                      <div class="col-xs-12">
+                        <input class="form-control input-lg" type="email" required="" placeholder="Email to Log in to Lazada">
+                      </div>
+                    </div>
+
+                    <div class="form-group">
+                      <div class="col-xs-12">
+                        <input class="form-control input-lg" type="Key" required="" placeholder="API Key">
+                      </div>
+                    </div>
+
+                    <div class="form-group text-center m-t-30">
+                      <div class="col-xs-12">
+                        <button type="submit" class="btn btn-primary waves-effect waves-light btn-lg m-b-5">Connect</button>
+                        <button type="submit" class="btn btn-primary waves-effect waves-light btn-lg m-b-5">Disconnect</button>
+                        <button type="submit" class="btn btn-default waves-effect waves-light btn-lg m-b-5">Cancel</button>
+                      </div>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
 
@@ -92,6 +140,9 @@
 
     data () {
       return {
+        ui: {
+          showSyncModal: false,
+        },
         list: {
           channel_resources: [],
           channels: [],
@@ -156,6 +207,11 @@
           console.error(err)
         }
       },
+
+      async syncModalToggle() {
+        this.ui.showSyncModal = ! this.ui.showSyncModal
+      },
+
     },
   }
 </script>
