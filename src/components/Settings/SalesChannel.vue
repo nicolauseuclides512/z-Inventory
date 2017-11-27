74067488adf185f7336604468b18968bef496346
Mulia Arifandi Nasution <mul14.net@gmail.com>
2017-11-27 14:29:32 +0700

    Sort channel resources

--- src/components/Settings/SalesChannel.vue
+++ src/components/Settings/SalesChannel.vue
@@ -133,7 +133,9 @@
 
       async salesChannelResource () {
         const res = await Axios.get(`my_channels/create`)
-        this.list.channel_resources = res.data.data.channels
+        const channel_resources = res.data.data.channels
+        const sortedData = _.sortBy(channel_resources, ['channel_name'])
+        this.list.channel_resources = sortedData
       },
 
       async salesChannel (params = {}) {
