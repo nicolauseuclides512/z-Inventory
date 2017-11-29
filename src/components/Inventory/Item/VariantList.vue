8ff74124f98094d95fad63e00e0da7d0f320107e
Mulia Arifandi Nasution <mul14.net@gmail.com>
2017-11-29 08:28:44 +0700

    Update VariantList

--- src/components/Inventory/Item/VariantList.vue
+++ src/components/Inventory/Item/VariantList.vue
@@ -11,9 +11,9 @@
                   <thead>
                   <tr>
                     <td>Item Name</td>
-                    <!--<td v-if="firstVariant.show">{{ firstVariant.name }}</td>-->
-                    <!--<td v-if="secondVariant.show">{{ secondVariant.name }}</td>-->
-                    <!--<td v-if="thirdVariant.show">{{ thirdVariant.name }}</td>-->
+                    <td v-if="Object.keys(item.item_attributes)[0]">{{ Object.keys(item.item_attributes)[0] }}</td>
+                    <td v-if="Object.keys(item.item_attributes)[1]">{{ Object.keys(item.item_attributes)[1] }}</td>
+                    <td v-if="Object.keys(item.item_attributes)[2]">{{ Object.keys(item.item_attributes)[2] }}</td>
                     <td>SKU</td>
                     <td>Price</td>
                     <td></td>
@@ -24,36 +24,39 @@
                     <td>
                       <input type="text" class="form-control form-white" v-model="v.item_name" title="">
                     </td>
-                    <td>
+                    <td v-if="Object.keys(item.item_attributes)[0]">
                       <input
                         type="text"
                         class="form-control form-white"
-                        disabled="disabled"
                         title=""
                       />
                     </td>
-                    <td>
-                      <pre v-text="list.items"></pre>
+                    <td v-if="Object.keys(item.item_attributes)[1]">
                       <input
                         type="text"
                         class="form-control form-white"
-                        disabled="disabled"
                         title=""
                       />
                     </td>
-                    <td>
+                    <td v-if="Object.keys(item.item_attributes)[2]">
                       <input
                         type="text"
                         class="form-control form-white"
-                        disabled="disabled"
                         title=""
                       />
                     </td>
-                    <td><input type="number" class="form-control form-white" v-model="v.code_sku"></td>
-                    <td><input type="text" class="form-control form-white" v-model="v.sales_rate"></td>
+                    <td><input type="number" class="form-control form-white" v-model="v.code_sku" title=""></td>
+                    <td><input type="text" class="form-control form-white" v-model="v.sales_rate" title=""></td>
                     <td>
                       <button type="button" @click="removeItem(v)" class="btn btn-danger btn-custom waves-effect">
                         <i class="ion-trash-b"></i>
+                      </button>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td colspan="10">
+                      <button type="button" @click="addMoreVariant" class="btn btn-default btn-custom waves-effect">
+                        Add more variant
                       </button>
                     </td>
                   </tr>
@@ -87,8 +90,8 @@
     data () {
       return {
         list: {
-          items: []
-        }
+          items: [],
+        },
       }
     },
 
@@ -97,7 +100,19 @@
     },
 
     methods: {
-      //
+
+      addMoreVariant () {
+        this.list.items.push({
+          item_name: this.item.item_name,
+          code_sku: this.item.code_sku,
+          sales_rate: this.item.sales_rate,
+        })
+      },
+
+      async removeItem (item) {
+        this.$emit('remove', item)
+      },
+
     },
   }
 </script>
