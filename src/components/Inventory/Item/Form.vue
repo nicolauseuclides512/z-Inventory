157837cc494d2c2b3f1ff73de041ae188cc5de47
Mulia Arifandi Nasution <mul14.net@gmail.com>
2018-02-06 12:16:31 +0700

    Change track inventory boolean to STRING

--- src/components/Inventory/Item/Form.vue
+++ src/components/Inventory/Item/Form.vue
@@ -72,13 +72,19 @@
             <div class="form-group form-general m-b-20">
               <div class="col-md-12 control-label text-left">
                 <div class="checkbox checkbox-success checkbox-inline">
-                  <input v-model="form.track_inventory" type="checkbox" id="inlineCheckbox1" value="option1"
-                         checked="checked">
-                  <label for="inlineCheckbox1">Track Inventory for this item</label>
+                  <input
+                    v-model="form.track_inventory"
+                    type="checkbox"
+                    id="track-inventory"
+                    true-value="true"
+                    false-value="false"
+                    checked="checked"
+                  />
+                  <label for="track-inventory">Track Inventory for this item</label>
                 </div>
               </div>
             </div>
-            <div id="show-stock" v-if="form.track_inventory">
+            <div id="show-stock" v-if="form.track_inventory == 'true'">
               <div class="form-group form-general m-b-20">
                 <label class="col-md-2 control-label text-left">Stock</label>
                 <div class="col-md-3">
@@ -248,7 +254,7 @@
           weight_unit: 'gr',
           code_sku: '',
           barcode: '',
-          track_inventory: true,
+          track_inventory: 'true',
           inventory_stock: 0,
           inventory_stock_warning: 0,
           category_id: null,
