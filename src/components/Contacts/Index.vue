4acfceaf086e13a6e5c2c856733cdcce86271c97
Jehan Afwazi A <jee.archer@gmail.com>
2018-02-14 17:40:40 +0700

    update contact | rearrange tag position

--- src/components/Contacts/Index.vue
+++ src/components/Contacts/Index.vue
@@ -2,6 +2,113 @@
   <div>
     <!-- Full list -->
 
+    <div class="container full-width-header bt-1 p-b-10">
+      <div class="row">
+        <div class="col-md-12 col-sm-12 col-xs-12">
+
+          <!-- Hide when one or more checkboxes are checked -->
+          <div v-show="!isChecked">
+            <a href="javascript:void(0);" id="filter-menu" class="dropdown-toggle pull-left page-title"
+               data-toggle="dropdown" aria-expanded="false">
+              <h4 :title="'Filter by ' + displayActiveCurrentFilter">
+                {{ displayActiveCurrentFilter }} Contacts <span class="caret"></span>
+              </h4>
+            </a>
+            <ul class="dropdown-menu" role="menu" style="top: 35px;">
+              <li class="dropdown-header">FILTER BY</li>
+              <li :class="{ active: currentFilter === 'all' }">
+                <a href="javascript:void(0);" id="filter-by-all" @click="filterContact('all')">All</a>
+              </li>
+              <li :class="{ active: currentFilter === 'customer' }">
+                <a href="javascript:void(0);" id="filter-by-customer" @click="filterContact('customer')">Customers</a>
+              </li>
+              <li :class="{ active: currentFilter === 'vendor' }">
+                <a href="javascript:void(0);" id="filter-by-vendor" @click="filterContact('vendor')">Vendors</a>
+              </li>
+              <li :class="{ active: currentFilter === 'dropshipper' }">
+                <a href="javascript:void(0);" id="filter-by-dropshipper" @click="filterContact('dropshipper')">Dropshippers</a>
+              </li>
+              <li :class="{ active: currentFilter === 'reseller' }">
+                <a href="javascript:void(0);" id="filter-by-reseller"
+                   @click="filterContact('reseller')">Resellers</a>
+              </li>
+              <li class="divider"></li>
+              <!--<li :class="{ active: currentFilter == 'active' }">-->
+              <!--<a href="javascript:void(0);" id="filter-by-active" @click="filterContact('active')">Active</a>-->
+              <!--</li>-->
+              <!--<li :class="{ active: currentFilter == 'inactive' }">-->
+              <!--<a href="javascript:void(0);" id="filter-by-inactive" @click="filterContact('inactive')">Inactive</a>-->
+              <!--</li>-->
+            </ul>
+            <div class="pull-right">
+              <router-link :to="{ name: 'contact.create' }" href="javascript:void(0);"
+                           class="btn btn-info waves-light m-b-5" id="create-new">
+                <span>New </span> <i class="ion-plus"></i>
+              </router-link>
+              <button id="sort-menu" class="btn btn-default waves-effect waves-light m-b-5" data-toggle="dropdown"
+                      aria-expanded="false">
+                <i class="fa fa-bars"></i>
+              </button>
+              <ul class="dropdown-menu" role="menu" style="top: 35px;">
+                <li class="dropdown-header">SORT BY</li>
+                <li :class="{ active: currentSortColumn === 'display_name' }">
+                  <a href="javascript:void(0);" id="sort-by-name" @click="sortContactsBy('display_name')">Name</a>
+                </li>
+                <li :class="{ active: currentSortColumn === 'company_name' }">
+                  <a href="javascript:void(0);" id="sort-by-company" @click="sortContactsBy('company_name')">Company
+                    Name</a>
+                </li>
+                <li :class="{ active: currentSortColumn === 'created_at' }">
+                  <a href="javascript:void(0);" id="sort-by-created" @click="sortContactsBy('created_at')">Created
+                    Time</a>
+                </li>
+                <li :class="{ active: currentSortColumn === 'updated_at' }">
+                  <a href="javascript:void(0);" id="sort-by-updated" @click="sortContactsBy('updated_at')">Last
+                    Modified Time</a>
+                </li>
+                <!--<li class="divider"></li>-->
+                <!--<li><a href="javascript:void(0);"><i class="md-file-download"></i> Import Customers</a></li>-->
+                <!--<li><a href="javascript:void(0);"><i class="md-file-download"></i> Import Vendors</a></li>-->
+                <!--<li class="divider"></li>-->
+                <!--<li><a href="javascript:void(0);"><i class="md-file-upload"></i> Export Customers</a></li>-->
+                <!--<li><a href="javascript:void(0);"><i class="md-file-upload"></i> Export Vendors</a></li>-->
+                <li class="divider"></li>
+                <li><a href="javascript:void(0);" @click="refresh"><i class="md-refresh"></i> Refresh List</a></li>
+              </ul>
+            </div>
+          </div>
+
+          <!-- Show when one or more checkboxes are checked -->
+          <div class="col-md-12 col-sm-12 col-xs-12" id="mark_active" v-show="isChecked" style="display: none;">
+            <div class="pull-left">
+              <!--<a href="javascript:void(0);"-->
+              <!--id="mark-as-active"-->
+              <!--class="btn btn-default waves-effect waves-light m-b-5"-->
+              <!--@click="markAsActive(checkedContacts)"-->
+              <!--&gt;-->
+              <!--Mark as Active-->
+              <!--</a>-->
+              <div class="btn-group">
+                <button id="more-actions" class="btn btn-default waves-effect waves-light m-b-5"
+                        data-toggle="dropdown" aria-expanded="false">
+                  More Actions <i class="caret"></i>
+                </button>
+                <ul class="dropdown-menu" role="menu">
+                  <!--<li><a href="javascript:void(0);" id="mark-as-inactive" @click="markAsInactive(checkedContacts)">Mark as Inactive</a></li>-->
+                  <li><a href="javascript:void(0);" id="destroy" @click="destroy(checkedContacts)">Delete</a></li>
+                </ul>
+              </div>
+            </div>
+            <div class="pull-right pt-10">
+              <a href="javascript:void(0);" id="close-action-bar" @click="clearCheckedContacts()" class="close-btn">
+                <i class="ion-android-close"></i>
+              </a>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
     <div v-if="!contacts.length" class="text-center" style="color: #a9a9a9; padding-top: 60px;">
       <i class="fa fa-5x fa-address-book"></i>
       <div class="lead" style="padding: 30px 0 5px;">
@@ -17,177 +124,68 @@
       </div>
     </div>
 
-    <div v-if="contacts.length">
-      <div class="container full-width-header bt-1 p-b-10">
-        <div class="row">
-          <div class="col-md-12 col-sm-12 col-xs-12">
-
-            <!-- Hide when one or more checkboxes are checked -->
-            <div v-show="!isChecked">
-              <a href="javascript:void(0);" id="filter-menu" class="dropdown-toggle pull-left page-title"
-                 data-toggle="dropdown" aria-expanded="false">
-                <h4 :title="'Filter by ' + displayActiveCurrentFilter">
-                  {{ displayActiveCurrentFilter }} Contacts <span class="caret"></span>
-                </h4>
-              </a>
-              <ul class="dropdown-menu" role="menu" style="top: 35px;">
-                <li class="dropdown-header">FILTER BY</li>
-                <li :class="{ active: currentFilter === 'all' }">
-                  <a href="javascript:void(0);" id="filter-by-all" @click="filterContact('all')">All</a>
-                </li>
-                <li :class="{ active: currentFilter === 'customer' }">
-                  <a href="javascript:void(0);" id="filter-by-customer" @click="filterContact('customer')">Customers</a>
-                </li>
-                <li :class="{ active: currentFilter === 'vendor' }">
-                  <a href="javascript:void(0);" id="filter-by-vendor" @click="filterContact('vendor')">Vendors</a>
-                </li>
-                <li :class="{ active: currentFilter === 'dropshipper' }">
-                  <a href="javascript:void(0);" id="filter-by-dropshipper" @click="filterContact('dropshipper')">Dropshippers</a>
-                </li>
-                <li :class="{ active: currentFilter === 'reseller' }">
-                  <a href="javascript:void(0);" id="filter-by-reseller"
-                     @click="filterContact('reseller')">Resellers</a>
-                </li>
-                <li class="divider"></li>
-                <!--<li :class="{ active: currentFilter == 'active' }">-->
-                <!--<a href="javascript:void(0);" id="filter-by-active" @click="filterContact('active')">Active</a>-->
-                <!--</li>-->
-                <!--<li :class="{ active: currentFilter == 'inactive' }">-->
-                <!--<a href="javascript:void(0);" id="filter-by-inactive" @click="filterContact('inactive')">Inactive</a>-->
-                <!--</li>-->
-              </ul>
-              <div class="pull-right">
-                <router-link :to="{ name: 'contact.create' }" href="javascript:void(0);"
-                             class="btn btn-info waves-light m-b-5" id="create-new">
-                  <span>New </span> <i class="ion-plus"></i>
-                </router-link>
-                <button id="sort-menu" class="btn btn-default waves-effect waves-light m-b-5" data-toggle="dropdown"
-                        aria-expanded="false">
-                  <i class="fa fa-bars"></i>
-                </button>
-                <ul class="dropdown-menu" role="menu" style="top: 35px;">
-                  <li class="dropdown-header">SORT BY</li>
-                  <li :class="{ active: currentSortColumn == 'display_name' }">
-                    <a href="javascript:void(0);" id="sort-by-name" @click="sortContactsBy('display_name')">Name</a>
-                  </li>
-                  <li :class="{ active: currentSortColumn == 'company_name' }">
-                    <a href="javascript:void(0);" id="sort-by-company" @click="sortContactsBy('company_name')">Company
-                      Name</a>
-                  </li>
-                  <li :class="{ active: currentSortColumn == 'created_at' }">
-                    <a href="javascript:void(0);" id="sort-by-created" @click="sortContactsBy('created_at')">Created
-                      Time</a>
-                  </li>
-                  <li :class="{ active: currentSortColumn == 'updated_at' }">
-                    <a href="javascript:void(0);" id="sort-by-updated" @click="sortContactsBy('updated_at')">Last
-                      Modified Time</a>
-                  </li>
-                  <!--<li class="divider"></li>-->
-                  <!--<li><a href="javascript:void(0);"><i class="md-file-download"></i> Import Customers</a></li>-->
-                  <!--<li><a href="javascript:void(0);"><i class="md-file-download"></i> Import Vendors</a></li>-->
-                  <!--<li class="divider"></li>-->
-                  <!--<li><a href="javascript:void(0);"><i class="md-file-upload"></i> Export Customers</a></li>-->
-                  <!--<li><a href="javascript:void(0);"><i class="md-file-upload"></i> Export Vendors</a></li>-->
-                  <li class="divider"></li>
-                  <li><a href="javascript:void(0);" @click="refresh"><i class="md-refresh"></i> Refresh List</a></li>
-                </ul>
-              </div>
-            </div>
-
-            <!-- Show when one or more checkboxes are checked -->
-            <div class="col-md-12 col-sm-12 col-xs-12" id="mark_active" v-show="isChecked" style="display: none;">
-              <div class="pull-left">
-                <!--<a href="javascript:void(0);"-->
-                <!--id="mark-as-active"-->
-                <!--class="btn btn-default waves-effect waves-light m-b-5"-->
-                <!--@click="markAsActive(checkedContacts)"-->
-                <!--&gt;-->
-                <!--Mark as Active-->
-                <!--</a>-->
-                <div class="btn-group">
-                  <button id="more-actions" class="btn btn-default waves-effect waves-light m-b-5"
-                          data-toggle="dropdown" aria-expanded="false">
-                    More Actions <i class="caret"></i>
-                  </button>
-                  <ul class="dropdown-menu" role="menu">
-                    <!--<li><a href="javascript:void(0);" id="mark-as-inactive" @click="markAsInactive(checkedContacts)">Mark as Inactive</a></li>-->
-                    <li><a href="javascript:void(0);" id="destroy" @click="destroy(checkedContacts)">Delete</a></li>
-                  </ul>
-                </div>
-              </div>
-              <div class="pull-right pt-10">
-                <a href="javascript:void(0);" id="close-action-bar" @click="clearCheckedContacts()" class="close-btn">
-                  <i class="ion-android-close"></i>
-                </a>
-              </div>
-            </div>
-
-          </div>
-        </div>
-      </div>
-      <div class="container p-0">
-        <div class="row sahito-list">
-          <div class="col-md-12 p-0">
-            <div class="sahito-list-contact border-1 table-responsive">
-              <table class="table table-hover default-table sahito-list-contact--table table-striped">
-                <thead>
-                <tr>
-                  <th class="col-checkbox">
-                    <div class="checkbox checkbox-single checkbox-success">
-                      <input type="checkbox" id="checkAll" @click="checkAll">
-                      <label></label>
-                    </div>
-                  </th>
-                  <th>
-                    <a href="#" id="sort-title-name" @click="sortContactsBy('display_name')">Name</a>
-                    <i class="fa fa-sort-asc" v-if="ascendingSort && currentSortColumn === 'display_name'"></i>
-                    <i class="fa fa-sort-desc" v-if="! ascendingSort && currentSortColumn === 'display_name'"></i>
-                  </th>
-                  <th>
-                    <a href="#" id="sort-title-company" @click="sortContactsBy('company_name')">Company Name</a>
-                    <i class="fa fa-sort-asc" v-if="ascendingSort && currentSortColumn === 'company_name'"></i>
-                    <i class="fa fa-sort-desc" v-if="! ascendingSort && currentSortColumn === 'company_name'"></i>
-                  </th>
-                  <th>
-                    <a href="#" id="sort-title-email" @click="sortContactsBy('email')">Email</a>
-                    <i class="fa fa-sort-asc" v-if="ascendingSort && currentSortColumn === 'email'"></i>
-                    <i class="fa fa-sort-desc" v-if="! ascendingSort && currentSortColumn === 'email'"></i>
-                  </th>
-                  <th>
-                    <a href="#" id="sort-title-work-phone" @click="sortContactsBy('phone')">Work Phone</a>
-                    <i class="fa fa-sort-asc" v-if="ascendingSort && currentSortColumn === 'phone'"></i>
-                    <i class="fa fa-sort-desc" v-if="! ascendingSort && currentSortColumn === 'phone'"></i>
-                  </th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr v-for="(c, index) in contacts" :class="{ active: isChecked }">
-                  <td class="col-checkbox">
-                    <div class="checkbox checkbox-single checkbox-success">
-                      <input type="checkbox" :id="'list-' + index" :value="c.contact_id" v-model="checkedContacts">
-                      <label></label>
-                    </div>
-                  </td>
-                  <td>
-                    <router-link :to="{ name: 'contact.edit', params: {id: c.contact_id }}">
+    <div v-if="contacts.length" class="container p-0">
+      <div class="row sahito-list">
+        <div class="col-md-12 p-0">
+          <div class="sahito-list-contact border-1 table-responsive">
+            <table class="table table-hover default-table sahito-list-contact--table table-striped">
+              <thead>
+              <tr>
+                <th class="col-checkbox">
+                  <div class="checkbox checkbox-single checkbox-success">
+                    <input type="checkbox" id="checkAll" @click="checkAll">
+                    <label></label>
+                  </div>
+                </th>
+                <th>
+                  <a href="#" id="sort-title-name" @click="sortContactsBy('display_name')">Name</a>
+                  <i class="fa fa-sort-asc" v-if="ascendingSort && currentSortColumn === 'display_name'"></i>
+                  <i class="fa fa-sort-desc" v-if="! ascendingSort && currentSortColumn === 'display_name'"></i>
+                </th>
+                <th>
+                  <a href="#" id="sort-title-company" @click="sortContactsBy('company_name')">Company Name</a>
+                  <i class="fa fa-sort-asc" v-if="ascendingSort && currentSortColumn === 'company_name'"></i>
+                  <i class="fa fa-sort-desc" v-if="! ascendingSort && currentSortColumn === 'company_name'"></i>
+                </th>
+                <th>
+                  <a href="#" id="sort-title-email" @click="sortContactsBy('email')">Email</a>
+                  <i class="fa fa-sort-asc" v-if="ascendingSort && currentSortColumn === 'email'"></i>
+                  <i class="fa fa-sort-desc" v-if="! ascendingSort && currentSortColumn === 'email'"></i>
+                </th>
+                <th>
+                  <a href="#" id="sort-title-work-phone" @click="sortContactsBy('phone')">Work Phone</a>
+                  <i class="fa fa-sort-asc" v-if="ascendingSort && currentSortColumn === 'phone'"></i>
+                  <i class="fa fa-sort-desc" v-if="! ascendingSort && currentSortColumn === 'phone'"></i>
+                </th>
+              </tr>
+              </thead>
+              <tbody>
+              <tr v-for="(c, index) in contacts" :class="{ active: isChecked }">
+                <td class="col-checkbox">
+                  <div class="checkbox checkbox-single checkbox-success">
+                    <input type="checkbox" :id="'list-' + index" :value="c.contact_id" v-model="checkedContacts">
+                    <label></label>
+                  </div>
+                </td>
+                <td>
+                  <router-link :to="{ name: 'contact.edit', params: {id: c.contact_id }}">
                       <span v-if="c.display_code === 1 || !c.display_code">
                         {{ c.salutation_id | display_salutation }} {{ c.first_name }} {{ c.last_name }}
                       </span>
-                      <span v-if="c.display_code === 2">
+                    <span v-if="c.display_code === 2">
                         {{ c.company_name }}
                       </span>
-                    </router-link>
-                  </td>
-                  <td>{{ c.company_name }}</td>
-                  <td>{{ c.email }}</td>
-                  <td>{{ c.phone == 'null' ? '' : c.phone }}</td>
-                </tr>
-                </tbody>
-              </table>
-            </div>
+                  </router-link>
+                </td>
+                <td>{{ c.company_name }}</td>
+                <td>{{ c.email }}</td>
+                <td>{{ c.phone == 'null' ? '' : c.phone }}</td>
+              </tr>
+              </tbody>
+            </table>
           </div>
-          <pagination :page-context="paginate" :result="contacts" @updated="updatePagination"></pagination>
         </div>
+        <pagination :page-context="paginate" :result="contacts" @updated="updatePagination"></pagination>
       </div>
     </div>
 
