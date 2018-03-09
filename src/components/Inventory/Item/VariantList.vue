b591f10d969d5504b92019a19af112c10ed518d8
Mulia Arifandi Nasution <mul14.net@gmail.com>
2018-03-09 17:22:42 +0700

    Remove some tags

--- src/components/Inventory/Item/VariantList.vue
+++ src/components/Inventory/Item/VariantList.vue
@@ -1,72 +1,58 @@
 <template>
-  <div class="">
-    <div class="">
-      <div class="">
-        <!--<h5 class="title">Variant</h5>-->
-        <div class="form-horizontal">
-          <div class="">
-            <div class="" style="padding-top: 30px">
-              <div class="table-responsive">
-                <table class="table table-bordered">
-                  <thead>
-                  <tr>
-                    <td>Item Name</td>
-                    <td>SKU</td>
-                    <td v-if="Object.keys(item.item_attributes)[0]">{{ Object.keys(item.item_attributes)[0] }}</td>
-                    <td v-if="Object.keys(item.item_attributes)[1]">{{ Object.keys(item.item_attributes)[1] }}</td>
-                    <td v-if="Object.keys(item.item_attributes)[2]">{{ Object.keys(item.item_attributes)[2] }}</td>
-                    <td>Stock</td>
-                    <td>Price</td>
-                    <td></td>
-                  </tr>
-                  </thead>
-                  <tbody>
-                  <tr v-for="v in list.items">
-                    <td>
-                      <input type="text" class="form-control form-white" v-model="v.item_name" title="">
-                    </td>
-                    <td><input type="text" class="form-control form-white" v-model="v.code_sku" title=""></td>
-                    <td v-if="Object.keys(item.item_attributes)[0]">
-                      <input
-                        type="text"
-                        class="form-control form-white"
-                        title=""
-                      />
-                    </td>
-                    <td v-if="Object.keys(item.item_attributes)[1]">
-                      <input
-                        type="text"
-                        class="form-control form-white"
-                        title=""
-                      />
-                    </td>
-                    <td v-if="Object.keys(item.item_attributes)[2]">
-                      <input
-                        type="text"
-                        class="form-control form-white"
-                        title=""
-                      />
-                    </td>
-                    <td><input type="text" class="form-control form-white" v-model="v.inventory_stock" title="Stock"></td>
-                    <td><input type="text" class="form-control form-white" v-model="v.sales_rate" title=""></td>
-                    <td>
-                      <button type="button" @click="updateItem(v)" class="btn btn-info btn-custom waves-effect">
-                        <i class="fa fa-check"></i>
-                      </button>
-                      <button type="button" @click="removeItem(v)" class="btn btn-danger btn-custom waves-effect">
-                        <i class="ion-trash-b"></i>
-                      </button>
-                    </td>
-                  </tr>
-                  </tbody>
-                </table>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+  <!--<h5 class="title">Variant</h5>-->
+  <table class="table table-bordered">
+    <thead>
+    <tr>
+      <td>Item Name</td>
+      <td>SKU</td>
+      <td v-if="Object.keys(item.item_attributes)[0]">{{ Object.keys(item.item_attributes)[0] }}</td>
+      <td v-if="Object.keys(item.item_attributes)[1]">{{ Object.keys(item.item_attributes)[1] }}</td>
+      <td v-if="Object.keys(item.item_attributes)[2]">{{ Object.keys(item.item_attributes)[2] }}</td>
+      <td>Stock</td>
+      <td>Price</td>
+      <td></td>
+    </tr>
+    </thead>
+    <tbody>
+    <tr v-for="v in list.items">
+      <td>
+        <input type="text" class="form-control form-white" v-model="v.item_name" title="">
+      </td>
+      <td><input type="text" class="form-control form-white" v-model="v.code_sku" title=""></td>
+      <td v-if="Object.keys(item.item_attributes)[0]">
+        <input
+          type="text"
+          class="form-control form-white"
+          title=""
+        />
+      </td>
+      <td v-if="Object.keys(item.item_attributes)[1]">
+        <input
+          type="text"
+          class="form-control form-white"
+          title=""
+        />
+      </td>
+      <td v-if="Object.keys(item.item_attributes)[2]">
+        <input
+          type="text"
+          class="form-control form-white"
+          title=""
+        />
+      </td>
+      <td><input type="text" class="form-control form-white" v-model="v.inventory_stock" title="Stock"></td>
+      <td><input type="text" class="form-control form-white" v-model="v.sales_rate" title=""></td>
+      <td>
+        <button type="button" @click="updateItem(v)" class="btn btn-info btn-custom waves-effect">
+          <i class="fa fa-check"></i>
+        </button>
+        <button type="button" @click="removeItem(v)" class="btn btn-danger btn-custom waves-effect">
+          <i class="ion-trash-b"></i>
+        </button>
+      </td>
+    </tr>
+    </tbody>
+  </table>
 </template>
 
 <script>
