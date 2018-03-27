8b97d5709de4fcb6aa9f345daf65e5ef94616e21
yosefyudha <yose@zuragan.com>
2018-03-27 16:46:42 +0700

    Wording on rrror message when delete contact

--- src/components/Contacts/Index.vue
+++ src/components/Contacts/Index.vue
@@ -232,7 +232,8 @@
   import Axios from 'axios'
   import {getParameterByName} from 'src/helpers'
   import _ from 'lodash'
-  import {responseOk} from '../../helpers'
+  //import {responseOk} from '../../helpers'
+  import {responseOk, swal_error, swal_success} from 'src/helpers'
   import Salutation from '../../helpers/Salutation'
 
   export default {
@@ -353,10 +354,16 @@
 
           try {
             const res = await Axios.delete('contacts?ids=' + queryString)
-            swal_success(res)
-            this.clearCheckedContacts()
-            this.contacts = []
-            this.getList()
+
+            if (!responseOk(res.data.code)) {
+              Alert.error('Delete Contact(s) Failed. Some contacts related to some Sales Orders')
+            } else {
+              //swal_success(res)
+              Alert.success('Contact(s) deleted')
+              this.clearCheckedContacts()
+              this.contacts = []
+              this.getList()
+            }
           } catch (e) {
             console.error(e)
             Alert.error('Failed to delete this contact. Delete all sales orders that contain this contact first.')
