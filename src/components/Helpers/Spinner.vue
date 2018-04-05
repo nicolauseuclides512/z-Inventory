d5ef95c8c5caa3cec3d392f90eacd07303a3357e
pais rebotak <rebotak@gmail.com>
2018-04-05 11:17:56 +0700

    add Helpers: Spinner

---
+++ src/components/Helpers/Spinner.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="text-center" style="color: #a9a9a9; padding-top: 60px;">
+    <i class="fa fa-5x fa-spin fa-circle-o-notch"></i>
+    <div class="lead" style="padding: 30px 0 5px;">
+      Loading
+      <br>
+      <small>Please wait...</small>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Spinner'
+  }
+</script>
+
+<style scoped>
+</style>
