2f5bc814777131e827b87445fdb7b01c3ef4f301
Rebotak <rebotak@gmail.com>
2018-04-12 12:22:54 +0700

    add global styling for display flex and display block

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,19 @@
 <template>
   <router-view></router-view>
 </template>
+
+<style lang="scss">
+  .d-flex{
+    display: flex;
+    align-content: center;
+    justify-content: center;
+    flex-direction: row;
+  }
+  .d-block{
+    display: block;
+    margin: 0 auto;
+    width: 100%;
+    position: relative;
+    overflow: hidden;
+  }
+</style>
