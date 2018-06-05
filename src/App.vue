648ab931fcfe1d6a69da1bbccf295f9b7d268315
rebotak <rebotak@gmail.com>
2018-06-05 10:35:56 +0700

    inline pagination

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,4 @@
 </template>
 
 <style lang="scss">
-  .d-flex{
-    display: flex;
-    align-content: center;
-    justify-content: center;
-    flex-direction: row;
-  }
-  .d-block{
-    display: block;
-    margin: 0 auto;
-    width: 100%;
-    position: relative;
-    overflow: hidden;
-  }
 </style>
